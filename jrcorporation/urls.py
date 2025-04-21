d5332eb068b5ca3808d0a512bb17e93945c55488
tohidrupok <tohidrupok@gmail.com>
2025-04-21 18:40:47 +0600

    remove text

--- jrcorporation/urls.py
+++ jrcorporation/urls.py
@@ -5,13 +5,13 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views. i would like go that . that why i can not go to the return  
+    1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home, importnt but it's make sence some enargy, we wnna go back from here. 
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home') , like then zero, from today tho another day. 
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home') ,
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path by path. 
+    1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
