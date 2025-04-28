2ab4ca9916527f388fcb970b3b0fc427a86c8822
tohidrupok <tohidrupok@gmail.com>
2025-04-28 18:20:39 +0600

    error solve and backend views issue solve

--- core/urls.py
+++ core/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('items/', views.item_list, name='item_list'),
     path('items/create/', views.item_create, name='item_create'),
     path('items/<int:pk>/edit/', views.item_update, name='item_update'),
-    path('items/<int:pk>/delete/', views.item_delete, name='item_delete'), 
+    path('items/<int:pk>/delete/', views.item_delete, name='item_delete'),  
     
     
     # Project
