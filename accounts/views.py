b97878c377bfd275d1fdcbc3bb35a294e25d5d5d
tohidrupok <tohidrupok@gmail.com>
2025-05-17 17:45:28 +0600

    home page work done

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,10 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
 
+from core.models import Project, Memo, ManpowerMemo
+from finance.models import Transaction
+from django.db.models import Sum, F, ExpressionWrapper, IntegerField
+
 def register_view(request):
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
@@ -15,12 +19,54 @@ def register_view(request):
     return render(request, "registration/register.html", {"form": form}) 
 
 
+def get_total_receivables_from_running_projects():
+    receivables = Project.objects.filter(
+        status='RUNNING',
+        final_bill__gt=0  # Exclude 0 and None
+    ).annotate(
+        due=ExpressionWrapper(F('final_bill') - F('current_paid'), output_field=IntegerField())
+    ).aggregate(
+        total_due=Sum('due')
+    )
+    return receivables['total_due'] or 0 
+
+def get_total_market_payables():
+    payables = Memo.objects.annotate(
+        due=ExpressionWrapper(F('amount') - F('payment_balance'), output_field=IntegerField())
+    ).aggregate(
+        total_due=Sum('due')
+    )
+    return payables['total_due'] or 0
+
+def get_total_manpower_payables():
+    payables = ManpowerMemo.objects.annotate(
+        due=ExpressionWrapper(F('amount') - F('payment_balance'), output_field=IntegerField())
+    ).aggregate(
+        total_due=Sum('due')
+    )
+    return payables['total_due'] or 0 
+
+
 @login_required
 def home_view(request):
 
     if request.user.is_staff:
-        return render(request, 'home_staff.html')  
-    else:   
-        return render(request, 'home.html')    
+        projects = Project.objects.filter(status='RUNNING').count()
+        all_projects = Project.objects.all().count()
+        
+        all_transaction= Transaction.objects.all().count()
+        
+        
+        #pawna tk gula
+        total_receivables = get_total_receivables_from_running_projects() 
+        
+        #dena tk gula
+        total_market_due = get_total_market_payables()
+        total_manpower_due = get_total_manpower_payables()
+        total_all_due = total_market_due + total_manpower_due 
+        
+        return render(request, 'home_staff.html', {'projects': projects, 'all_projects': all_projects, 'total_receivables': total_receivables, 'total_all_due':total_all_due, 'all_transaction': all_transaction }) 
     
+    else:
+        return render(request, 'home.html')
     
--- core/models.py
+++ core/models.py
@@ -94,7 +94,7 @@ class Memo(models.Model):
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name='memos')
     items = models.ManyToManyField(Item, related_name='memos')
     amount = models.PositiveIntegerField(default=0)
-    payment_balance = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
+    payment_balance = models.PositiveIntegerField(default=0, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
